03a1b67933b367172a8ec0e52c1e888af66d30aa
booldook <booldook@gmail.com>
2021-09-02 14:41:18 +0900

    router 테스트 완료

--- routes/sql/index.js
+++ routes/sql/index.js
@@ -11,12 +11,14 @@ router.get('/list', async (req, res, next) => {
 	res.status(200).json(r)
 })
 
+// let sql = `INSERT INTO books SET title='${title}', writer='${writer}', content='${content}'`
 router.get('/create', async (req, res, next) => {
 	let title = '홍길동전'
 	let writer = '허균'
 	let content = '아버지를 아버지라...'
-	let sql = `INSERT INTO books SET title='${title}', writer='${writer}', content='${content}'`
-	let r = await connect.execute(sql)
+	let sql = 'INSERT INTO books SET title=?, writer=?, content=?'
+	let values = [title, writer, content]
+	let r = await pool.execute(sql, values)
 	res.status(200).json(r)
 })
 

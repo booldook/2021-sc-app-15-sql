11644de2f420f42e9a8af4f163704af185b8c57b
booldook <booldook@gmail.com>
2021-09-02 13:18:04 +0900

    sql insert select 테스트 완료

--- routes/sql/index.js
+++ routes/sql/index.js
@@ -3,8 +3,32 @@ const express = require('express')
 const router = express.Router()
 const { error } = require('../../modules/util')
 
-router.use((req, res, next) => {
-	
+const mysql = require('mysql2')
+const connection = mysql.createConnection({
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME
+});
+
+router.get('/list', (req, res, next) => {
+	let sql = 'SELECT * FROM books'
+	const onResult = (err, r) => {
+		res.status(200).json(r)
+	}
+	connection.query(sql, onResult)
+})
+
+router.get('/create', (req, res, next) => {
+	let title = '홍길동전'
+	let writer = '허균'
+	let content = '아버지를 아버지라...'
+	let sql = `INSERT INTO books SET title='${title}', writer='${writer}', content='${content}'`
+	const onResult = (err, r) => {
+		res.status(200).json(r)
+	}
+	connection.query(sql, onResult)
 })
 
 module.exports = router

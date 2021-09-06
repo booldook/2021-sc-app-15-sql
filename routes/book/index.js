4dee4fb7dcde832041fed78327ad0f2e2db972e8
booldook <booldook@gmail.com>
2021-09-06 14:55:25 +0900

    update완료 / CRUD구현

--- routes/book/index.js
+++ routes/book/index.js
@@ -4,16 +4,18 @@ const router = express.Router()
 const { error } = require('../../modules/util')
 
 const formRouter = require('./form-router')
-const saveRouter = require('./save-router')
 const listRouter = require('./list-router')
 const viewRouter = require('./view-router')
+const createRouter = require('./create-router')
+const updateRouter = require('./update-router')
 const deleteRouter = require('./delete-router')
 
-router.post('/', saveRouter)													// POST: 저장
-router.delete('/', deleteRouter)											// DELETE: 삭제
-router.use('/form', formRouter)												// HTML: 글작성(수정)페이지
-router.use(['/', '/list', '/list/:page'], listRouter)	// HTML/GET: 리스트페이지
-router.use('/view', viewRouter)												// HTML/GET: 상세페이지
+router.post('/', createRouter)		// POST: 저장
+router.put('/', updateRouter)			// UPDATE: 수정
+router.delete('/', deleteRouter)	// DELETE: 삭제
+router.use('/form', formRouter)		// HTML: 글작성(수정)페이지
+router.use('/view', viewRouter)		// HTML/GET: 상세페이지
+router.use('/', listRouter)				// HTML/GET: 리스트페이지
 
 module.exports = router
 

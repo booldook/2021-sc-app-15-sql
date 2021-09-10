1089fd1f70e33d052fc8d159d3306c73a5e9cc93
booldook <booldook@gmail.com>
2021-09-10 12:39:35 +0900

    lang-mw

--- middlewares/lang-mw.js
+++ middlewares/lang-mw.js
@@ -2,11 +2,10 @@ module.exports = (req, res, next) => {
 	const langs = ['ko', 'en']
 	const urls = req.url.split('/')
 	if(urls[1] && langs.includes(urls[1])) {
-		var [lang] = urls.splice(1, 1) // /en/book -> /book
+		var [lang] = urls.splice(1, 1) 			// /en/book -> /book
 		req.url = urls.join('/')
 	}
 	req.lang = require('../modules/lang-init')(lang || 'ko')
-	req.app.locals = req.app.locals ?  { ...req.app.locals, ...req.lang } : { ...req.lang }
-	console.log(req.app.locals)
+	req.app.locals = { ...req.app.locals, ...req.lang }
 	next()
 }

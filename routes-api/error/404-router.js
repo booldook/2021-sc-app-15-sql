432715e8c7eaa3bd1f6cbbca39714cde72cd5681
booldook <booldook@gmail.com>
2021-09-15 11:07:06 +0900

    routes-api

---
+++ routes-api/error/404-router.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const { error } = require('../../modules/util')
+
+router.use((req, res, next) => {
+	// console.log(req.url)
+	next( error(404) )
+})
+
+
+module.exports = router

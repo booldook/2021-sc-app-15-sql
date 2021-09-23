3ca78093292908ca13d9de55b2e6f2c969a4d2e2
booldook <booldook@gmail.com>
2021-09-23 16:05:38 +0900

    validation 작업중

--- public/js/auth/form.js
+++ public/js/auth/form.js
@@ -17,49 +17,111 @@ var passwd2Txt = document.querySelector('.passwd2');
 var usernameTxt = document.querySelector('.username');
 var emailTxt = document.querySelector('.email');
 
-document.saveForm.addEventListener('submit', onSubmit)
+
+f.addEventListener('submit', onSubmit)
+useridEl.addEventListener('blur', verifyUserid)
+passwdEl.addEventListener('keyup', verifyPasswd)
+passwdEl.addEventListener('blur', verifyPasswd)
+passwd2El.addEventListener('keyup', verifyPasswd2)
+passwd2El.addEventListener('blur', verifyPasswd2)
+passwd2El.addEventListener('blur', verifyPasswdEqual)
+usernameEl.addEventListener('keyup', verifyUsername)
+usernameEl.addEventListener('blur', verifyUsername)
+emailEl.addEventListener('keyup', verifyEmail)
+emailEl.addEventListener('blur', verifyEmail)
+
 function onSubmit(e) {
 	e.preventDefault();
-	var userid = useridEl.value.trim();
-	var passwd = passwdEl.value.trim();
-	var passwd2 = passwd2El.value.trim();
-	var username = usernameEl.value.trim();
-	var email = emailEl.value.trim();
-
 	
+	var isUserid = verifyUserid();
+	var isPasswd = verifyPasswd();
+	var isPasswd2 = verifyPasswd2();
+	var isPasswdEqual = verifyPasswdEqual();
+	var isUsername = verifyUsername();
+	var isEmail = verifyEmail();
+	
+	if(isUserid && isPasswd  && isPasswd2 && isPasswdEqual && isUsername && isEmail) f.submit();
+}
+
+function verifyUserid() {
+	var userid = useridEl.value.trim();
+	verifyReset(useridEl, useridTxt);
+	if(userid === '' || userid.length < 6 || userid.length > 24) {
+		verifyFalse(useridEl, useridTxt, userid === '' ? ERR.ID_NULL : ERR.ID_LEN);
+		return false;
+	}
+	else {
+		verifyTrue(useridEl, useridTxt, ERR.ID_OK);
+		return true;
+	}
+}
+
+function verifyPasswd() {
+	var passwd = passwdEl.value.trim();
+	useridEl.classList.remove('error');
+	useridEl.classList.remove('active');
+	useridTxt.classList.remove('error');
+	useridTxt.innerHTML = '';
 	if(passwd === '') {
 		return false;
 	}
 	if(passwd.length < 6 || passwd.length > 24) {
 		return false;
 	}
+	return true;
+}
+
+function verifyPasswd2() {
+	var passwd2 = passwd2El.value.trim();
 	if(passwd2 === '') {
 		return false;
 	}
 	if(passwd2.length < 6 || passwd2.length > 24) {
 		return false;
 	}
+	return true;
+}
+
+function verifyPasswdEqual() {
+	var passwd = passwdEl.value.trim();
+	var passwd2 = passwd2El.value.trim();
 	if(passwd !== passwd2) {
 		return false;
 	}
+	return true;
+}
+
+function verifyUsername() {
+	var username = usernameEl.value.trim();
 	if(username === '') {
 		return false;
 	}
+	return true;
+}
+
+function verifyEmail() {
+	var email = emailEl.value.trim();
 	if(email === '') {
 		return false;
 	}
+	return true;
 }
 
-function verifyUserId() {
-	if(userid === '') {
-		useridTxt.classList.add('error');
-		useridTxt.innerHTML = ERR.ID_NULL;
-		useridEl.classList.add('error');
-		useridEl.focus();
-		return false;
-	}
-	if(userid.length < 6 || userid.length > 24) {
-		return false;
-	}
-	return true;
+function verifyReset(el, elTxt) {
+	el.classList.remove('error');
+	el.classList.remove('active');
+	elTxt.classList.remove('error');
+	elTxt.innerHTML = '';
+}
+
+function verifyFalse(el, elTxt, msg) {
+	elTxt.classList.add('error');
+	elTxt.innerHTML = msg;
+	el.classList.add('error');
+	el.focus();
+}
+
+function verifyTrue(el, elTxt, msg) {
+	el.classList.add('active');
+	elTxt.innerHTML = msg;
 }

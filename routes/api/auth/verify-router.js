5a32c56ba22e0560eb456b123b4364f08b409205
booldook <booldook@gmail.com>
2021-09-24 11:22:47 +0900

    userid 중복 확인 완료

--- routes/api/auth/verify-router.js
+++ routes/api/auth/verify-router.js
@@ -2,28 +2,26 @@ const path = require('path')
 const express = require('express')
 const router = express.Router()
 const { error } = require('../../../modules/util')
-const { pool } = require('../../../modules/mysql-init')
+const { mysql, pool } = require('../../../modules/mysql-init')
 
 router.get('/verify', async (req, res, next) => {
 	// userid, email 중복 검증
 	let sql
 	let key = req.query.key
 	let value = req.query.value
-	
 	try {
 		if(value.includes("'") || value.includes('"')) {
-			res.status(401).json({ error: '특수문자가 포함되어 있습니다.' })
+			res.status(400).json({ msg: '특수문자가 포함되어 있습니다.' })
 		}
 		else {
-			sql = ` SELECT ${key} FROM users WHERE ${key}=? `
-			console.log(sql)
+			sql = ` SELECT * FROM users WHERE ${key} = ? `
 			const [rs] = await pool.execute(sql, [value])
 			console.log(rs)
 			res.status(200).json({ isUsed: rs.length ? true : false })
 		}
 	}
 	catch(err) {
-		res.status(500).json(err)
+		res.status(500).json({ msg: '서버오류 입니다. 다시 시도해 주세요.', err })
 	}
 })
 

68a76b0503f6aae1412360b41166a3fbf3186618
booldook <booldook@gmail.com>
2021-09-24 10:27:56 +0900

    validation 적용중

--- routes/api/auth/verify-router.js
+++ routes/api/auth/verify-router.js
@@ -4,12 +4,27 @@ const router = express.Router()
 const { error } = require('../../../modules/util')
 const { pool } = require('../../../modules/mysql-init')
 
-router.get('/userid', (req, res, next) => {
-	// userid 중복 검증
-})
-
-router.get('/email', (req, res, next) => {
-	// email 중복 검증
+router.get('/verify', async (req, res, next) => {
+	// userid, email 중복 검증
+	let sql
+	let key = req.query.key
+	let value = req.query.value
+	
+	try {
+		if(value.includes("'") || value.includes('"')) {
+			res.status(401).json({ error: '특수문자가 포함되어 있습니다.' })
+		}
+		else {
+			sql = ` SELECT ${key} FROM users WHERE ${key}=? `
+			console.log(sql)
+			const [rs] = await pool.execute(sql, [value])
+			console.log(rs)
+			res.status(200).json({ isUsed: rs.length ? true : false })
+		}
+	}
+	catch(err) {
+		res.status(500).json(err)
+	}
 })
 
 module.exports = router

de5d0f5f1702358ecc5f2c444f60f003f008a394
booldook <booldook@gmail.com>
2021-09-27 15:07:13 +0900

    session-mw.js

--- middlewares/session-mw.js
+++ middlewares/session-mw.js
@@ -1,10 +1,24 @@
 const session = require('express-session')
+const MySQLStore = require('express-mysql-session')(session)
+
+const storeOptions = {
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME,
+	expiration: 86400000,
+}
 
 const expressSession = session({
 	secret: process.env.COOKIE_SALT,
 	resave: false,
 	saveUninitialized: true,
-	cookie: { secure: false }
+	store: new MySQLStore(storeOptions),
+	cookie: { 
+		secure: false,
+		httpOnly: true,
+	},
 })
 
 module.exports = app => {
